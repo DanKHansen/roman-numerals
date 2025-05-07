bc58b83f841fa301fa92cf15b84178192322bf0d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-07 12:43:07 +0200

    solution

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,2 +1,7 @@
 object RomanNumerals:
-   def roman(i:Int): String = ???
+   def roman(n: Int): String =
+      val m = List("", "M", "MM", "MMM")
+      val c = List("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
+      val x = List("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
+      val i = List("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+      m(n / 1000) + c((n % 1000) / 100) + x((n % 100) / 10) + i(n % 10)
