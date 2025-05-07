a714601623268e1538675bd987c96d7fdbd7fcae
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-07 13:08:18 +0200

    refactor

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,7 +1,7 @@
 object RomanNumerals:
-   def roman(n: Int): String =
-      val m = List("", "M", "MM", "MMM")
-      val c = List("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
-      val x = List("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
-      val i = List("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
-      m(n / 1000) + c((n % 1000) / 100) + x((n % 100) / 10) + i(n % 10)
+   def roman(n: Int): String = Array(
+     Array("", "M", "MM", "MMM"),
+     Array("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),
+     Array("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
+     Array("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+   ).zipWithIndex.map { case (arr, idx) => arr((n / Math.pow(10, 3 - idx).toInt) % 10) }.mkString
