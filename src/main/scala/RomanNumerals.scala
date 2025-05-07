82e78b28795c6bc3dd07a21750b732473706ea3b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-07 13:09:25 +0200

    refactor

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -4,4 +4,4 @@ object RomanNumerals:
      Array("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),
      Array("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
      Array("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
-   ).zipWithIndex.map { case (arr, idx) => arr((n / Math.pow(10, 3 - idx).toInt) % 10) }.mkString
+   ).zipWithIndex.map { case (a, i) => a((n / Math.pow(10, 3 - i).toInt) % 10) }.mkString
